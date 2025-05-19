d8ee236a7a4e5a8953dc4c80aa0e3c6af4e6b953
Tristan <119907414+plots-TH@users.noreply.github.com>
2025-05-19 18:44:12 -0400

    render track list for each public playlsit on explore public page

--- music_app-fe/src/components/PublicPlaylistCard.jsx
+++ music_app-fe/src/components/PublicPlaylistCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function PublicPlaylistCard({ publicPlaylist }) {
   return (
@@ -6,7 +7,7 @@ function PublicPlaylistCard({ publicPlaylist }) {
       <h2>{publicPlaylist.title}</h2>
 
       {/* show 1st track cover. if 0 tracks, dont render image tag */}
-      {publicPlaylist.cover_url && (
+      {publicPlaylist.tracks.length > 0 && (
         <img
           src={publicPlaylist.cover_url}
           alt={publicPlaylist.title}
@@ -15,6 +16,17 @@ function PublicPlaylistCard({ publicPlaylist }) {
       )}
 
       {publicPlaylist.description && <p>{publicPlaylist.description}</p>}
+
+      <div>
+        {publicPlaylist.tracks &&
+          publicPlaylist.tracks.map((track) => (
+            <div key={track.track_id}>
+              <Link to={`/track/${track.track_id}`}>
+                <strong>{track.track_title}</strong> by {track.track_artist}
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
